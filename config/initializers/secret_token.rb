b7031d1c5fde09de739f85593bff1b2056bbd89e
snebel <nebelout@gmail.com>
2014-01-12 20:11:49 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MapShare::Application.config.secret_key_base = '8aae78875a9de70213209d5480f361011c81af95ae6277a342f2653e6cac6fc969884b357aaa3a4ded45b21ef4a8f19889f8c3c0b47e35d212d140a704b9d6ba'
